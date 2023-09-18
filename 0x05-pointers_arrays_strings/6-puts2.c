0ea3ec11a96da5dd1e77e603a21e65ed869e71b1
Z-ajamy <abdorahman0283@gmail.com>
2023-09-18 10:48:01 -0700

    0

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,25 @@
- #include "main.h"
+#include "main.h"
 
 /**
- * puts2 - Write a function that prints every other character of a string, 
- *         starting with the first character, followed by a new line.
+ * puts2 - Prints every other character of a string, starting with the first character,
+ *         followed by a new line.
+ * @s: This pointer points to a character or string.
  *
- * @s: This pointer point to a char or string
- *
- *
+ * Description: This function iterates through the string pointed to by 's' and
+ *              prints every other character starting with the first character.
+ *              It then prints a newline character to terminate the line.
  */
-
 void puts2(char *s)
 {
-	int a;
+    int a;
 
-	for (a = 0; s[a] != '\0'; a++)
-	{
-		if (a % 2 == 0)
-		{
-			_putchar(s[a]);
-		}
-	}
-	_putchar('\n');
+    for (a = 0; s[a] != '\0'; a++)
+    {
+        if (a % 2 == 0)
+        {
+            _putchar(s[a]);
+        }
+    }
+    _putchar('\n');
 }
+
