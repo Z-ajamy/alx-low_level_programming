715e5ca21e4655e9585fdcb668dd77d528c5c8e0
Z-ajamy <abdorahman0283@gmail.com>
2023-10-03 07:53:42 -0700

    the README.md file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+
+/*
+ * print "Programming is like building a multilingual puzzle
+ *	using puts instead of printf
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
