d809301214c52aed6aba2c0a892348c29907600b
Z-ajamy <abdorahman0283@gmail.com>
2023-10-03 10:08:22 -0700

    the README.md file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main: the enter point
+ * printf: print data
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
