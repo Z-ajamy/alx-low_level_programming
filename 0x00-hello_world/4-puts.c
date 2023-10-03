330903d6038e84bbb6d70b1e4f645e057f2b4dcc
Z-ajamy <abdorahman0283@gmail.com>
2023-10-03 08:30:11 -0700

    the README.md file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 
 /**
- *main - Prints quote
+ * main - Prints a quote
  *
- *return: zero to success
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
