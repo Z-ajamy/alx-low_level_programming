1fa788c2478496bc58185b75a1cbea5615be332d
Z-ajamy <abdorahman0283@gmail.com>
2023-10-03 14:23:43 -0700

    the README.md file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ * main - Entry point of the program
+ * Return: 0 (success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
