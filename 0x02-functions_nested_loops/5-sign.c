e9dea9491299eebaab17e592b536e97fafb27eea
Z-ajamy <abdorahman0283@gmail.com>
2023-10-04 11:53:46 -0700

    the README.md file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number.
+ *
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+
+	int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+, 1");
+		return (1);
+	}
+	else if (n < 0)
+	{
+		printf("-, -1");
+		return (-1);
+	}
+	else
+	{
+		printf("0, 0");
+		return (0);
+	}
+
+}
