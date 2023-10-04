5380ebd8c4714cfc108e2db771a936e208cdbfd2
Z-ajamy <abdorahman0283@gmail.com>
2023-10-04 11:18:11 -0700

    the README.md file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character.
- *@c: parameter int
- *Return:1 if c is a letter, lowercase or uppercase
- *Return: 0 otherwise
+ * @c: parameter int
+ * Return: 1 if c is a letter, lowercase or uppercase
  */
 
 	int _isalpha(int c)
 {
-	if (c <= 122 && c >= 97 || c <= 90 && C >= 65)
+	if (c <= 122 && c >= 97 || c <= 90 && c >= 65)
 	{
 	return (1);
 	}
