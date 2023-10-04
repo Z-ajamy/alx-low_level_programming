f6e0dfd48c6eba9e43cfe58149986938c82a807a
Z-ajamy <abdorahman0283@gmail.com>
2023-10-03 17:12:34 -0700

    the README.md file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
