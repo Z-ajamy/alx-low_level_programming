752eb5877bf17fb6d37e20fa18df1c3ed028965b
Z-ajamy <abdorahman0283@gmail.com>
2023-10-04 12:00:26 -0700

    the README.md file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@
 {
 	if (n > 0)
 	{
-		printf("+, 1");
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("-, -1");
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		printf("0, 0");
+		_putchar('0');
 		return (0);
 	}
 
