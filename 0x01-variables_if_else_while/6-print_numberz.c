eeadca4ef700543929978293ba79d25a2aaad842
Z-ajamy <abdorahman0283@gmail.com>
2023-10-03 18:10:15 -0700

    the README.md file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry
+ *
+ *
+ *Return: 0 (done)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
