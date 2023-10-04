1792791ea0228d2475e41df70bea08ee84393293
Z-ajamy <abdorahman0283@gmail.com>
2023-10-04 10:59:01 -0700

    the README.md file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 /**
  * _islower - lower or not??
  *
- *
- *
+ * @c: is a parameter
+ *Return: 0 lower 1 not
  */
 	int _islower(int c)
 
 {
-	if (c <= 97 && c >= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
