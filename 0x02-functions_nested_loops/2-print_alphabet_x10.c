832b97db43e2785058e72c3f881a8523d23ea668
Z-ajamy <abdorahman0283@gmail.com>
2023-10-04 08:09:43 -0700

    the README.md file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10.c - It prints the lowercase alphabet 10 times
+ * each time on a special line
+ *
+ */
+
+	void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int n = 97;
+
+		while (n <= 122)
+		{
+			_putchar(n);
+			n++;
+		}
+		i++;
+	}
+}
