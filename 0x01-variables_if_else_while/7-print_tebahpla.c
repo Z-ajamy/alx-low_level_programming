fcd0d6463ccc811f7cac7097065879ecaa04fb58
Z-ajamy <abdorahman0283@gmail.com>
2023-10-03 18:13:07 -0700

    the README.md file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry
+ *
+ *
+ *Return: 0 (done)
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+	i--;
+	}
+	printf("\n");
+	return (0);
+}
