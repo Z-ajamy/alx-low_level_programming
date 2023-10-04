a0be04a71db3bc744a3bc294b9080327406cb9d5
Z-ajamy <abdorahman0283@gmail.com>
2023-10-04 06:07:51 -0700

    the README.md file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
+ *main - Entry
+ *
+ *
+ *Return: 0 (done)
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
