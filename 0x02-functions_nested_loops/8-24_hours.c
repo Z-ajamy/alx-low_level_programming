53537e94467b9c1d3db47644ee1c8542785a5cdb
Z-ajamy <abdorahman0283@gmail.com>
2023-10-05 05:52:45 -0700

    the README.md file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ * Return: void
+ */
+	void jack_bauer(void)
+{
+		int n = 0;
+
+	while (n <= 2)
+	{
+		int m = 0;
+
+		while (m <= 3)
+		{
+			int l = 0;
+
+			while (l <= 5)
+			{
+				int p = 0;
+
+				while (p <= 9)
+				{
+					_putchar('0' + n);
+					_putchar('0' + m);
+					_putchar(':');
+					_putchar('0' + l);
+					_putchar('0' + p);
+					_putchar('\n');
+				p++;
+								}
+			l++;
+						}
+		m++;
+				}
+	n++;
+		}
+}
