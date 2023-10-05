9f6ee008b3fd2e4a13a82dabb448fadf7b9ea9c5
Z-ajamy <abdorahman0283@gmail.com>
2023-10-05 13:14:23 -0700

    the README.md file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ *
+ *
+ */
+	void times_table(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		int m = 0;
+
+		while (m <= 9)
+		{
+			printf("%d,  ", n * m);
+		m++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
