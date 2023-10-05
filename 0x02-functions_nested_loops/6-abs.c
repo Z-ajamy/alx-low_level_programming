c0f2e6e12f26f7b831bc0224925418d911e4ae47
Z-ajamy <abdorahman0283@gmail.com>
2023-10-05 01:00:34 -0700

    the README.md file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character.
+ * 
+ * Return: 1 if c is a letter, lowercase or uppercase
+ */
+	int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (-n)
+}
