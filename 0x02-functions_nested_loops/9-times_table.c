2a633f038cd83e3bfc40c273c66f6298ab1c6bb2
Z-ajamy <abdorahman0283@gmail.com>
2023-10-09 20:15:48 +0300

    0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,6 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * times_table - prints the 9 times table, starting with 0
- *
- * Return (0) when success
- *
  */
 	void times_table(void)
 {
@@ -17,15 +13,11 @@
 		while (m <= 9)
 		{
 		int	R = n * m;
-/* اول-عمود-اصفار*/
+		
 		if (R == 0 && m == 0)
 		{
 			_putchar(R + '0');
 		}
-/**
- * كل-الي-مكون-من
- * -رقم--معادا-اول-2-في-العمود-الاخير
- */
 		else if (R < 10 && m != 9)
 			{
 				_putchar(',');
@@ -33,7 +25,6 @@
 				_putchar(' ');
 				_putchar(R + '0');
 			}
-/*كل-الي-مكون-من-رقمين--معادا-العمود-الاخير*/
 			else if (R >= 10 && m != 9)
 			{
 				_putchar(',');
@@ -41,10 +32,8 @@
 				_putchar(R / 10 + '0');
 				_putchar(R % 10 + '0');
 			}
-			/*العمود-الاخير*/
 			else if (m == 9)
 			{
-			/*كل-العمود-عدا-اخر-2*/
 				if (R >= 10)
 				{
 				_putchar(',');
@@ -52,7 +41,6 @@
 				_putchar(R / 10 + '0');
 				_putchar(R % 10 + '0');
 				}
-				/*اول-2-في-العمود*/
 				else if (R < 10)
 				{
 					_putchar(',');
@@ -67,4 +55,3 @@
 		n++;
 	}
 }
-
