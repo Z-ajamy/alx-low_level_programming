e4fbf54cc2e118939b1452c2f4ba0ca65f216fd0
Z-ajamy <abdorahman0283@gmail.com>
2023-10-09 08:01:19 -0700

    the README.md file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,9 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * times_table - prints the 9 times table, starting with 0
  *
- *
+ * Return (0) when success
  *
  */
 	void times_table(void)
@@ -15,10 +16,55 @@
 
 		while (m <= 9)
 		{
-			printf("%d,  ", n * m);
-		m++;
+		int	R = n * m;
+/* اول-عمود-اصفار*/
+		if (R == 0 && m == 0)
+		{
+			_putchar(R + '0');
+		}
+/**
+ * كل-الي-مكون-من
+ * -رقم--معادا-اول-2-في-العمود-الاخير
+ */
+		else if (R < 10 && m != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(R + '0');
+			}
+/*كل-الي-مكون-من-رقمين--معادا-العمود-الاخير*/
+			else if (R >= 10 && m != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(R / 10 + '0');
+				_putchar(R % 10 + '0');
+			}
+			/*العمود-الاخير*/
+			else if (m == 9)
+			{
+			/*كل-العمود-عدا-اخر-2*/
+				if (R >= 10)
+				{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(R / 10 + '0');
+				_putchar(R % 10 + '0');
+				}
+				/*اول-2-في-العمود*/
+				else if (R < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(R % 10 + '0');
+				}
+			}
+			m++;
 		}
 		_putchar('\n');
 		n++;
 	}
 }
+
