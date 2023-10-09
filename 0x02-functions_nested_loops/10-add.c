aa1cd7b810b4dbe6a33c2faf29ad1b53663d3235
Z-ajamy <abdorahman0283@gmail.com>
2023-10-09 20:37:44 +0300

    0

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,13 @@
-nbkjn
+#include "main.h"
+/**
+ * add - Add two numbers
+ * @n: is a number
+ * @m: is a number
+ * Return: The sum of n and m
+*/
+int add(int n, int m)
+{
+int R;
+    R = n + m;
+    return (R);
+}
