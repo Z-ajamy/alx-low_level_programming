86c81c355f23bb20237f5f653e90ea2a5d666038
Z-ajamy <abdorahman0283@gmail.com>
2023-10-11 01:15:22 +0300

    0

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - prints the name of the file
  *
- * Return: Always 0 (Success)
  * 
-*/
-int main(void) {
-    printf(__FILE__ "\n");
-    return 0;
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+
+return (0);
 }
