cfed4d809572098bba0acd17cb3b37f23d11ca28
Z-ajamy <abdorahman0283@gmail.com>
2023-10-12 19:45:38 +0300

    0

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include<stdio.h>
 /**
  * _puts - a function that prints a
  * string, followed by a new line
@@ -9,7 +10,7 @@ void _puts(char *str)
 int i;
 	for(i = 0; str[i] != 0; i++)
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
