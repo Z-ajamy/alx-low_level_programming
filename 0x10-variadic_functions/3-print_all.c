8d17989f33256af772b4e5b9fe7fbb64a6041b25
Z-ajamy <abdorahman0283@gmail.com>
2023-10-12 21:06:35 +0300

    0

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,54 +1,49 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
- * print_all - Prints anything based on the provided format.
- * @format: A list of types of arguments passed to the function.
- * @...: The values to be printed.
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
-	char *str;
-	int num;
-	float num_float;
-	char *separator = "";
+	int i = 0;
+	char *str, *sep = "";
 
-	va_start(args, format); /* Initialize the argument list. */
+	va_list list;
 
-	while (format && format[i])
+	va_start(list, format);
+
+	if (format)
 	{
-		if (i > 0 && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
-			printf(", ");
-
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-				num = va_arg(args, int);
-				printf("%s%c", separator, num);
-				break;
-			case 'i':
-				num = va_arg(args, int);
-				printf("%s%d", separator, num);
-				break;
-			case 'f':
-				num_float = va_arg(args, double);
-				printf("%s%f", separator, num_float);
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s%s", separator, str);
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		separator = "";
-		i++;
 	}
 
-	va_end(args); /* Clean up the argument list. */
 
-	printf("\n"); /* Print a new line at the end. */
+	printf("\n");
+	va_end(list);
 }
