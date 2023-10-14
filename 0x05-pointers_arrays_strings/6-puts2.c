870e3389801c0ab273a8998cbc9d2d4704f96fc4
Z-ajamy <abdorahman0283@gmail.com>
2023-10-15 02:27:56 +0300

    0

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * 
+ * 
+ * 
+*/
+void puts2(char *str)
+{
+    int i;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        putchar(str[i]);
+        putchar('\n');
+    }
+    
+}
