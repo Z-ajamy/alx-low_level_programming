e47288dfa9923b677c4f0116e094415d5837774b
Z-ajamy <abdorahman0283@gmail.com>
2023-10-14 23:49:44 +0300

    0

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * 
+ * 
+ * 
+*/
+void _puts(char *str)
+{
+    int i;
+
+    for (i = 0; str[i] != '\n'; i++)
+    {
+        continue;
+    }
+    putchar(*str);
+    
+
+   
+}
