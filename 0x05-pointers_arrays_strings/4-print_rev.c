4226728bef75f0f5d14ebbbfaf7bd472db6e6463
Z-ajamy <abdorahman0283@gmail.com>
2023-10-15 02:17:35 +0300

    0

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,16 @@
 */
 void print_rev(char *s)
 {
-    int i;
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        continue;
-    }
-int n;
-    for (n = i-1; n >=0; n--)
-    {
-        putchar(s[n]);
-    }
+	int i;
+	int n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	
+	for (n = i-1; n >=0; n--)
+	{
+		putchar(s[n]);
+	}
 }
