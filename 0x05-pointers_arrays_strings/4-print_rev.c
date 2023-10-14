3ba5ad06205b278e47139b7031c49f30cca08442
Z-ajamy <abdorahman0283@gmail.com>
2023-10-15 00:27:02 +0300

    0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * 
+ * 
+*/
+void print_rev(char *s)
+{
+    int i;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        continue;
+    }
+    int n;
+    int c = i;
+    char *str;
+    str[c + 1];
+
+    for ( n = 0; n == (c + 1) ; n++)
+    {
+        if (n <= c)
+        {
+         str[n] = s[i];
+        }
+        else if (n > c)
+        {
+            str[n+1] = '\0';
+        }
+        i--;
+       
+    }
+    
+}
