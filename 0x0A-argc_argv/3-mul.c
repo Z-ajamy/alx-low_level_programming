5d4ec22402ad85dcc2da3768c6c11d83f1757699
Z-ajamy <abdorahman0283@gmail.com>
2023-10-15 14:56:58 +0300

    0

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * 
+ * 
+*/
+int main (int argc , char *argv[])
+{
+    int res;
+    if (argc == 3)
+    {
+        res = atoi(argv[1]) * atoi(argv[1]);
+        printf("%d", res)
+    }
+    
+}
