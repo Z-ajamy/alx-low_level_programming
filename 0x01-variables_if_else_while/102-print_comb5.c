6d15722357b311a561469e794e8a5dc559e817f0
Z-ajamy <abdorahman0283@gmail.com>
2023-10-19 00:02:53 +0300

    0

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - all possible different combinations of two digits.
+ * Numbers must be separated by ,, followed by a space
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+    
+
+putchar('\n');
+return (0);
+}
