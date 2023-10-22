acb673d8854b2a9d659282d22f2b2cfc04fce775
Z-ajamy <abdorahman0283@gmail.com>
2023-10-22 20:07:51 +0300

    prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,37 +5,32 @@
  */
 	void times_table(void)
 {
-    int i, n, c;
+	int i, n, c;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (n = 0; n < 10; n++)
-        {
-            c = n * i;
-            if (c < 10 && n != 9)
-            {
-                _putchar(c + 48);
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-            }
-            else if (c < 10 && n == 9)
-            {
-                 _putchar(c + 48);
-            }
-            if (c >= 10 && n != 9)
-            {
-                _putchar(c / 10 + 48);
-                _putchar(c % 10 + 48);
-                _putchar(',');
-                _putchar(' ');
-            }
-            if (c >= 10 && n == 9)
-            {
-                _putchar(c / 10 + 48);
-                _putchar(c % 10 + 48);
-            }
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			c = n * i;
+			if (c < 10 && n != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + 48);
+			}
+			else if (n == 0)
+			{
+				_putchar(c + 48);
+			}
+			if (c >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(c / 10 + 48);
+				_putchar(c % 10 + 48);
+			}
+		}
+		_putchar('\n');
+	}
 }
