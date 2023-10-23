b6b9eeb55dee213df57a1db64442c21ef61f7b6e
Z-ajamy <abdorahman0283@gmail.com>
2023-10-23 18:47:07 +0300

    0

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees 
- * @h: pointer to the first node
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
  *
  * Return: number of elements in the freed list
  */
