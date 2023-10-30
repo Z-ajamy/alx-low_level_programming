b5310ca9d4c8419ec715c90e11e91e85137178a0
Z-ajamy <abdorahman0283@gmail.com>
2023-10-31 01:26:52 +0200

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,24 +3,26 @@
 
 int lengh(char *str)
 {
-    int i;
-    if(str == NULL)
-        return (0);
-    for (i = 0; str[i] != '\0'; i++)
-    {}
-    return(i);
+	int i;
+
+	if (str == NULL)
+		return (0);
+	for (i = 0; str[i] != '\0'; i++)
+	{}
+	return (i);
 }
 
 
 char *_strcat(char *dest, char *src)
 {
-    int len1, len2, i;
-    len1 = lengh(dest);
-    len2 = lengh(src);
+	int len1, len2, i;
 
-    for (i = 0; i <= len2; i++)
-    {
-      dest[(len1-1)+i] = src[i];
-    }
-    return (dest);
+	len1 = lengh(dest);
+	len2 = lengh(src);
+
+	for (i = 0; i <= len2; i++)
+	{
+		dest[(len1 - 1) + i] = src[i];
+	}
+	return (dest);
 }
