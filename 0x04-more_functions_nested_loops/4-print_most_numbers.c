5e26da5c3beb76e547f9152f162f6dc01e2ad8ad
Z-ajamy <abdorahman0283@gmail.com>
2023-12-07 09:33:46 +0200

    from 0 to 9, followed by a new line.  Do not print 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,6 +8,7 @@
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		if (i == 2 || i == 4)
@@ -18,8 +19,6 @@ void print_most_numbers(void)
 		{
 			_putchar(48 + i);
 		}
-		
 	}
 	_putchar('\n');
 }
-	
