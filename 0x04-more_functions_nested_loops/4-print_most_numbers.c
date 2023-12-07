80a33a0771495f836611db47f1c01371f78e079e
Z-ajamy <abdorahman0283@gmail.com>
2023-12-07 09:31:23 +0200

    from 0 to 9, followed by a new line.  Do not print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - a function that prints the numbers,
+ * from 0 to 9, followed by a new line.
+ * Do not print 2 and 4
+*/
+void print_most_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(48 + i);
+		}
+		
+	}
+	_putchar('\n');
+}
+	
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-a function that prints the numbers, from 0 to 9, followed by a new line.
+from 0 to 9, followed by a new line.  Do not print 2 and 4
