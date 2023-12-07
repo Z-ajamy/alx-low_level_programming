587f40da6b947ed56570f9f1dda547095a900161
Z-ajamy <abdorahman0283@gmail.com>
2023-12-07 17:40:57 +0200

    _putchar

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-char *cap_string(char *);
-char *string_toupper(char *);
-void reverse_array(int *a, int n);
-int _strcmp(char *s1, char *s2);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
+
+int _putchar(char c);
+
 #endif
