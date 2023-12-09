25bedf68eab4ec7615d23f2abba78a95110da6e8
Z-ajamy <abdorahman0283@gmail.com>
2023-12-09 11:17:34 +0200

    a function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,8 @@
 /**
  * _memset - a function that fills memory with a constant byte.
  * @s: Pointer to the object to copy the character.
- * @b: The character to copy. It can be a character, a normal value as well a boolean value.
+ * @b: The character to copy. It can be a character,
+ * a normal value as well a boolean value.
  * @n: Number of bytes to copy.
  * Return: string
 */
