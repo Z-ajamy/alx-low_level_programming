272da981dbf563c4a9c3fe02bf944738e0f3cdf0
Z-ajamy <abdorahman0283@gmail.com>
2023-12-09 20:26:26 +0200

    0

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,7 +13,7 @@ char *_strpbrk(char *s, char *accept)
         {
             if (s[i] == accept[j])
             {
-                return (&s[i]);
+                return (s + i);
             }
         }
     }
