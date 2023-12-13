742efd489e26b7ecedb988f85bb1b5bbabe93824
Z-ajamy <abdorahman0283@gmail.com>
2023-12-13 17:52:28 +0200

    0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,9 +10,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
     int *p;
 
     if (nmemb == 0 || size == 0)
-        return (NULL);
+            return (NULL);
     p = (void *) malloc(nmemb * size);
-    
+    if (p == NULL)
+        return (NULL);
+    for (i = 0; i <= (nmemb * size); i++)
+        p[i] = 0;
     return (p);
-
 }
