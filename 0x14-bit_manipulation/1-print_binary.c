488a83828c04f12439e84d96603bdecdc728073f
Z-ajamy <abdorahman0283@gmail.com>
2023-12-14 19:58:01 +0200

    0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,17 +10,17 @@ void print_binary(unsigned long int n)
 
 	i = sizeof(n) * 8 - 1;
 	if (n == 0)
-		putchar('0');
+		_putchar('0');
 
 	while (i >= 0)
 	{
 		if ((n >> i) & 1)
 		{
-			putchar('1');
+			_putchar('1');
 			flag = 1;
 		}
 		else if (flag)
-			putchar('0');
+			_putchar('0');
 		i--;
 	}
 }
