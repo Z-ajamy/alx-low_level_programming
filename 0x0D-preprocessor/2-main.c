28e0bec096a9a0d109e07d8851c835c0bc979515
Z-ajamy <abdorahman0283@gmail.com>
2023-12-14 19:18:24 +0200

    a program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the name of the file it was compiled from.
+ *              Followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,1 +1,1 @@
-Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359
+a program that prints the name of the file it was compiled from, followed by a new line.
