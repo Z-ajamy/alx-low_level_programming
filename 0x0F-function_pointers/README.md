e376ae60a7d8c1635a915fb349cb29e173a5d298
Z-ajamy <abdorahman0283@gmail.com>
2024-03-25 02:47:17 +0200

    a function that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,1 @@
-a function that prints a name.
+a function that executes a function given as a parameter on each element of an array.
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
