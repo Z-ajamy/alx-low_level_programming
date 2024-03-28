66f00ee8646b0e6c8cdc3971a37c4c2d333cb1f6
Z-ajamy <abdorahman0283@gmail.com>
2024-03-28 22:47:05 +0200

    a function that prints anything.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,60 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+    va_list args;
+    int i = 0;
+    int n = strlen(format);
+    char *s;
+        char *seperator = "";
+
+    va_start(args, format);
+    while (i < n)
+    {
+        switch (format[i])
+        {
+        case 'c':
+            {
+            char c = va_arg(args, int);
+
+            printf("%s%c", seperator, c);
+            }
+            break;
+        case 'i':
+            {
+                int j = va_arg(args, int);
+
+                printf("%s%i", seperator, j);
+            }
+            break;
+        case 'f':
+            {
+                float f = va_arg(args, double);
+
+                printf("%s%f", seperator, f);
+            }
+            break;
+        case 's':
+            s = va_arg(args, char*);
+
+            if (!s)
+            {
+                s = "(nil)";
+            }
+            printf("%s%s", seperator, s);
+            break;
+        }
+        seperator = ", ";
+        i++;
+    }
+    va_end(args);
+    printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,1 +1,1 @@
-a function that prints strings, followed by a new line.
+a function that prints anything.
