053bbea6c4e691a29bf02f65a1dd755d15e83154
Z-ajamy <abdorahman0283@gmail.com>
2024-04-06 01:40:43 +0200

    a function that deletes the node at index index of a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 (Success), or -1 (Fail)
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *next, *ptr;
+	unsigned int i;
+
+	if (!head)
+	{
+		return (-1);
+	}
+	next = ptr = (*head);
+	if (index == 0)
+	{
+		(*head) = (*head)->next;
+		free(next);
+		return (1);
+	}
+	for (i = 0; i < index; i++)
+	{
+		ptr = ptr->next;
+		if (i == index - 1)
+		{
+			next->next = next->next->next;
+		}
+		next = next->next;
+	}
+	free(ptr);
+	return (1);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -52,3 +52,4 @@ new_node = malloc(sizeof(listint_t));
 a function that inserts a new node at a given position.
 a function that inserts a new node at a given position.
 a function that inserts a new node at a given position.
+a function that deletes the node at index index of a listint_t linked list.
