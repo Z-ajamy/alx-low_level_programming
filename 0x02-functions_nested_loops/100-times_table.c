f2eab61325066489357f0f3bdf7ce52db82bedf2
Z-ajamy <abdorahman0283@gmail.com>
2025-02-11 23:52:07 +0200

    Prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,56 +1,49 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The number up to which the times table should be printed.
+ *
+ * Description: If n is greater than 15 or less than 0, the function
+ * does not print anything. Otherwise, it prints a properly formatted
+ * multiplication table.
  */
 void print_times_table(int n)
 {
-int i, j, m;
-if (n >= 0 && n <= 15)
-{
-for (i = 0; i <= n ; i++)
-{
-for (j = 0; j <= n; j++)
-{
-m = i * j;
-if (n > 0 && n < 15)
-{
-if (m < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(m + 48);
-}
-else if (m > 9 && m < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((m / 10) + 48);
-_putchar((m % 10) + 48);
-}
-else if (m > 99 && m < 1000)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((m / 100) + 48);
-_putchar(((m / 10) % 10) + 48);
-_putchar((m % 10) + 48);
-}
-if (j == 0)
-{
-_putchar(m + 48);
-}
-}
-else if (n < 0 || n > 15)
-{
-continue;
-}
-}
-_putchar('\n');
-}
-}
+	int i, j, res;
+
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			res = i * j;
+
+			if (j != 0)
+			{
+				if ((res / 100) == 0)
+					_putchar(' ');
+				else
+					_putchar((res / 100) + '0');
+
+				if ((res / 10) == 0)
+					_putchar(' ');
+				else
+					_putchar(((res / 10) % 10) + '0');
+			}
+
+			_putchar((res % 10) + '0');
+
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
