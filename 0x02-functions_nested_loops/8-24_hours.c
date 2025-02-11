9ada55b97b2ab9e30d50c0c56a2a98bee0c37246
Z-ajamy <abdorahman0283@gmail.com>
2025-02-11 21:44:29 +0200

    from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,17 @@
  */
 	void jack_bauer(void)
 {
-	int i, n;
+	int i, j;
 
 	for (i = 0; i < 24; i++)
 	{
-		for (n = 0; n < 60; n++)
+		for (j = 0; j < 60; j++)
 		{
-			_putchar(i / 10 + 48);
-			_putchar(i % 10 + 48);
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
 			_putchar(':');
-			_putchar(n / 10 + 48);
-			_putchar(n % 10 + 48);
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
 			_putchar('\n');
 		}
 	}
