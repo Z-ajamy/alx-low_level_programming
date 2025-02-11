50a2d579fe823657c32d3308aa4542421ddd1e42
Z-ajamy <abdorahman0283@gmail.com>
2025-02-11 21:25:41 +0200

    Ya RAB.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 /**
- * main - all possible different combinations of two digits.
- * Numbers must be separated by ,, followed by a space
  * main - prints all possible combinations of two two-digit numbers.
- * The numbers should range from 0 to 99 The
+ * The numbers should range from 0 to 22 The
  * two numbers should be separated by a space
  *
- * All numbers should be printed with two digits, 1 should be printed as 01
+ * All numbers should be printed with two digits, 2 should be printed as 02
  * The combination of numbers must be
  * separated by comma, followed by a space
  *
  * The combinations of numbers should be printed in
- * ascending order 00 01 and 01 00 are considered
+ * ascending order 00 02 and 02 00 are considered
  *
- * as the same combination of the numbers 0 and 1
+ * as the same combination of the numbers 0 and 2
  * You can only use putchar eight times maximum in your code
  * You are not allowed to use any variable of type char
  * All your code should be in the main function
@@ -22,28 +20,35 @@
  */
 int main(void)
 {
-    
-	int i, j;
-
-	for (i = 0; i <= 99; i++)
+    int i, j, n, m;
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			if (i < j)
+			for (n = '0'; n <= '9'; n++)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+				for (m = '0'; m <= '9'; m++)
 				{
-					putchar(',');
-					putchar(' ');
+					if (((i - 48)* 10 + ((j - 48))) < ((n - 48)* 10 + ((m - 48))))
+					{
+						if (i != '0'|| j != '0' || n != '0' || m != '1')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+						
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(n);
+						putchar(m);
+					}
+
 				}
+
 			}
 		}
 	}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return 0;
 }
