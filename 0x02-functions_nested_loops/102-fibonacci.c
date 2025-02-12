59a70507bd0d006bc48bcee9724f914c0f68001f
Z-ajamy <abdorahman0283@gmail.com>
2025-02-13 00:28:53 +0200

    sum of even Fibonacci numbers up to 4,000,000.

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	unsigned long int i, a = 0, b = 1, res = 0;
+	unsigned long int i, a = 1, b = 0, res = 0;
 
 	for (i = 1; i <= 50; i++)
 	{
--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,26 @@
-#include"main.h"
+#include "main.h"
+#include <stdio.h>
+
 /**
- * 
- * 
-*/
+ * main - Finds the sum of even Fibonacci numbers up to 4,000,000.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	unsigned long int i, a = 1, b = 0, res = 0, sum = 0;
+
+	for (i = 1; res < 4000000; i++)
+	{
+		if (!(res % 2))
+		{
+			sum += res;
+		}
+		res += a;
+		a = b;
+		b = res;
+	}
+	printf("%ld\n", sum);
+
+	return (0);
+}
