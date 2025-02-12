2fefeb67bfea3088dee43b644828aa5f2768ff96
Z-ajamy <abdorahman0283@gmail.com>
2025-02-12 22:15:06 +0200

    ?

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,19 @@
 #include"main.h"
+#include <stdio.h>
 /**
  * 
  * 
 */
+int main(){
+    int i, res;
+    for (i = 1023; i > 0; i--)
+    {
+        if (i % 3 || i % 5)
+        {
+            res += i;
+        }
+        
+    }
+    printf("%d\n",res);
+    return 0;
+}
