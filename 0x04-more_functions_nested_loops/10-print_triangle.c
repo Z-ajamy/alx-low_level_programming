9eaba4cabbb90059b42a8310e6ec104acb25621f
Z-ajamy <abdorahman0283@gmail.com>
2025-02-16 18:07:15 +0200

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,28 +5,25 @@
  * print_triangle - Prints a right-aligned triangle of '#' characters.
  * @size: The size of the triangle.
  */
-
 void print_triangle(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-    
-    for (i = 1; i <= size; i++)
-    {
-        for (j = 1; j <= size; j++)
-        {
-            if (j <= (size - i))
-            {
-                _putchar(' ');
-            }
-            else
-                _putchar('#');
-        }
-        _putchar('\n');
-    }    
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (j <= (size - i))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
