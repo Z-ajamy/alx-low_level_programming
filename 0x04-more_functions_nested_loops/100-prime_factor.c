25df80e96a57a28f20a95614367ad3447e436eb7
Z-ajamy <abdorahman0283@gmail.com>
2025-02-17 00:19:11 +0200

    prime_factor of 612852475143

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,29 +8,29 @@
 
 int main(void)
 {
-    unsigned long int i, max, n;
+	unsigned long int i, max, n;
 
-    n = 612852475143;
+	n = 612852475143;
 
-    while (!(n % 2))
-    {
-        max = 2;
-        n /= 2;
-    }
+	while (!(n % 2))
+	{
+		max = 2;
+		n /= 2;
+	}
 
-    for (i = 3; i * i <= n; i += 2)
-    {
-        while (!(n % i))
-        {
-            max = i;
-            n /= i;
-        }
-    }
+	for (i = 3; i * i <= n; i += 2)
+	{
+		while (!(n % i))
+		{
+			max = i;
+			n /= i;
+		}
+	}
 
-    if (n > 2)
-        max = n;
+	if (n > 2)
+		max = n;
 
-    printf("%ld\n", max);
+	printf("%ld\n", max);
 
-    return (0);
+	return (0);
 }
