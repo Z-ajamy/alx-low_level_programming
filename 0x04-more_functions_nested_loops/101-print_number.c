1d71ad0d0bc2bd096b58cc14a312cc43d23773d8
Z-ajamy <abdorahman0283@gmail.com>
2025-02-17 13:21:34 +0200

    print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,36 +7,23 @@
 
 void print_number(int n)
 {
-	int i, len, k, s, a;
-
-	k = n;
+	int s;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		k *= -1;
-	}
-
-	len = 1;
-	i = 10;
-
-	while (n /= i)
-	{
-		len++;
+		n *= -1;
 	}
 
 	s = 1;
 
-	for (i = len; i > 0; i--)
+	while (n / s >= 10)
 	{
-		for (n = 1; n <= i - 1; n++)
-		{
-			s *= 10;
-		}
+		s *= 10;
+	}
 
-		a = k / s;
-		a %= 10;
-		_putchar(48 + a);
-		s = 1;
+	for (; s >= 1; s /= 10)
+	{
+		_putchar(48 + (n / s) % 10);
 	}
 }
