cc84f528c539ca7c20e5a785b9532e2a035c960f
Z-ajamy <abdorahman0283@gmail.com>
2025-03-07 06:48:13 +0200

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,7 @@
 int _atoi(char *s)
 {
 	int i = 0, sig = 1, n, count = 0;
-	int a, num = 0;
+	int num = 0;
 
 	while (1)
 	{
@@ -27,11 +27,13 @@ int _atoi(char *s)
 
 	for (; s[i] >= '0' && s[i] <= '9'; i++)
 	{
-		a = s[i] - '0';
-		num = (num * 10) + a;
+		if (num > (2147483647 - (s[i] - '0')) / 10)
+		{
+			return (sig == 1) ? 2147483647 : -2147483648;
+		}
+		num = (num * 10) + s[i] - '0';
 	}
 
 	num *= sig;
 	return (num);
-
 }
