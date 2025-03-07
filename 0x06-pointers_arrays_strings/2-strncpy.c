bdadee1aaf0d050a28eb83ecc9888f597e26ca45
Z-ajamy <abdorahman0283@gmail.com>
2025-03-07 13:56:24 +0200

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	/* نسخ الأحرف من src إلى dest */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	/* ملء باقي المساحة بـ '\0' في حال كانت src أقصر من n */
 	while (i < n)
 	{
 		dest[i++] = '\0';
