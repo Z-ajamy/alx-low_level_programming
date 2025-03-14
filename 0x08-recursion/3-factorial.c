65bde78346d44ceee8f5ad3d38f8c95fed0e2935
Z-ajamy <abdorahman0283@gmail.com>
2025-03-14 09:20:56 +0200

    the factorial of a given number.

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,15 +8,15 @@
  */
 int factorial(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-    
-    if (n == 0 || n == 1)
-    {
-        return (1);
-    }
-    n *= factorial(n - 1);
-    return (n);
+	if (n < 0)
+	{
+		return (-1);
+	}
+	
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	n *= factorial(n - 1);
+	return (n);
 }
