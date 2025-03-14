8976411d536362e09c6f96c4df91d097fa90d60b
Z-ajamy <abdorahman0283@gmail.com>
2025-03-14 16:12:53 +0200

    static_libs

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,66 @@
-Create the static library libmy.a
+# ALX Low-Level Programming - Static Libraries
+
+This directory contains exercises and projects related to static libraries in C as part of the ALX Software Engineering program. The objective of this project is to understand the concept of static libraries, how they are created, and how they are used in compiling programs.
+
+## Understanding Static Libraries
+
+A static library is a collection of object files that are grouped together into a single file, allowing code reuse and modularization. Unlike dynamic libraries, static libraries are linked at compile-time, meaning all required functions are included in the final executable.
+
+Static libraries have the `.a` (archive) extension and are commonly used for performance optimization and reducing dependencies during execution.
+
+## Project Files
+
+### Source Files
+These files contain implementations of common C standard functions that are included in the static library:
+- `0-isupper.c` - Checks for uppercase character.
+- `1-isdigit.c` - Checks for a digit (0 through 9).
+- `2-strlen.c` - Computes the length of a string.
+- `3-islower.c` - Checks for a lowercase character.
+- `3-puts.c` - Prints a string followed by a newline.
+- `4-isalpha.c` - Checks for an alphabetic character.
+- `5-strstr.c` - Locates a substring.
+- `6-abs.c` - Computes the absolute value of an integer.
+- `_putchar.c` - Custom implementation of `_putchar`.
+
+### Object Files
+Each `.c` file is compiled into a corresponding `.o` (object) file, which is then archived into a static library.
+
+### Static Library
+- `libmy.a` - The static library containing compiled object files.
+
+### Scripts and Headers
+- `main.h` - Header file containing function prototypes.
+- `create_static_lib.sh` - Shell script to automate the creation of the static library.
+- `main.c` - A sample main program to test the library.
+
+## Creating a Static Library
+To create a static library from the object files, use the `ar` command:
+
+```sh
+ar rc libmy.a *.o
+ranlib libmy.a
+```
+
+Alternatively, you can use the provided script:
+
+```sh
+./create_static_lib.sh
+```
+
+## Compiling a Program Using the Static Library
+Once the static library is created, compile any program that uses it as follows:
+
+```sh
+gcc -Wall -pedantic -Werror -Wextra main.c -L. -lmy -o output
+```
+
+## Why Use Static Libraries?
+- They improve performance by reducing runtime dependencies.
+- They allow for modular programming by organizing reusable functions.
+- They are useful when distributing software that does not require external dependencies.
+
+## Conclusion
+Understanding static libraries is essential for building efficient and reusable code in C. This project provides a hands-on approach to working with libraries, linking, and optimizing code compilation.
+
+## Author
+This project is part of the ALX Software Engineering program and is designed to enhance low-level programming skills.
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,25 +1,194 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Description: Header file containing function prototypes
+ * for various standard library functions, including
+ * character classification, string manipulation, and memory operations.
+ */
+
+/* Character classification functions */
+
+/**
+ * _isupper - Checks if a character is uppercase
+ * @c: The character to check
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
 int _isupper(int c);
-char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * _isdigit - Checks if a character is a digit (0-9)
+ * @c: The character to check
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
 int _isdigit(int c);
-char *_strcat(char *dest, char *src);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strncat(char *dest, char *src, int n);
-int _atoi(char *s);
-char *_strchr(char *s, char c);
-int _strlen(char *s);
-char *_strncpy(char *dest, char *src, int n);
+
+/**
+ * _islower - Checks if a character is lowercase
+ * @c: The character to check
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
 int _islower(int c);
-void _puts(char *str);
-int _strcmp(char *s1, char *s2);
-unsigned int _strspn(char *s, char *accept);
+
+/**
+ * _isalpha - Checks if a character is an alphabetic letter
+ * @c: The character to check
+ *
+ * Return: 1 if letter, 0 otherwise
+ */
 int _isalpha(int c);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @i: The integer to process
+ *
+ * Return: The absolute value of i
+ */
 int _abs(int i);
+
+/* String manipulation functions */
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: The string to measure
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s);
+
+/**
+ * _strcpy - Copies a string, including the null terminator
+ * @dest: The destination buffer
+ * @src: The source string
+ *
+ * Return: Pointer to dest
+ */
 char *_strcpy(char *dest, char *src);
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string to append
+ *
+ * Return: Pointer to dest
+ */
+char *_strcat(char *dest, char *src);
+
+/**
+ * _strncat - Concatenates two strings up to n bytes
+ * @dest: The destination string
+ * @src: The source string to append
+ * @n: Maximum number of bytes to append
+ *
+ * Return: Pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n);
+
+/**
+ * _strncpy - Copies up to n characters from src to dest
+ * @dest: The destination buffer
+ * @src: The source string
+ * @n: Maximum number of characters to copy
+ *
+ * Return: Pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n);
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Negative, zero, or positive if s1 < s2, s1 == s2, or s1 > s2
+ */
+int _strcmp(char *s1, char *s2);
+
+/* Memory manipulation functions */
+
+/**
+ * _memset - Fills memory with a constant byte
+ * @s: The memory area to fill
+ * @b: The byte to fill with
+ * @n: Number of bytes to fill
+ *
+ * Return: Pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * _memcpy - Copies memory from source to destination
+ * @dest: The destination buffer
+ * @src: The source buffer
+ * @n: Number of bytes to copy
+ *
+ * Return: Pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* String searching functions */
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string to search
+ * @c: The character to find
+ *
+ * Return: Pointer to first occurrence of c, or NULL if not found
+ */
+char *_strchr(char *s, char c);
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: The initial segment of the string
+ * @accept: The characters to match
+ *
+ * Return: Number of bytes in s consisting only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept);
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: The string to search
+ * @accept: The characters to look for
+ *
+ * Return: Pointer to first occurrence in s of any byte from accept, or NULL
+ */
+char *_strpbrk(char *s, char *accept);
+
+/**
+ * _strstr - Locates a substring within a string
+ * @haystack: The string to search
+ * @needle: The substring to find
+ *
+ * Return: Pointer to beginning of needle in haystack, or NULL
+ */
+char *_strstr(char *haystack, char *needle);
+
+/* Miscellaneous functions */
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
+ *
+ * Return: The converted integer value
+ */
+int _atoi(char *s);
+
+/**
+ * _puts - Prints a string followed by a newline
+ * @str: The string to print
+ */
+void _puts(char *str);
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success, 1. On error, -1 is returned
+ */
 int _putchar(char c);
 
-#endif
+#endif /* MAIN_H */
