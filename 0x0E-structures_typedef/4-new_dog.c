f5fbe25ddd3388c1cf5e43bef894f6c55e42c8ec
Z-ajamy <abdorahman0283@gmail.com>
2025-03-16 17:51:50 +0200

    My name is Ghost, I am 4.75, and my owner is Jon Snow

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,16 +1,62 @@
 #include "dog.h"
-int counrstr(char *str);
-void _strcpy(char *s, char *str);
 
+/**
+ * counrstr - Computes the length of a string.
+ * @str: Pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+int counrstr(char *str)
+{
+    int i;
+
+    if (!str)
+        return (0);
+
+    for (i = 0; str[i] != 0; i++)
+        continue;
+
+    return (i);
+}
+
+/**
+ * _strcpy - Copies a string from source to destination.
+ * @s: Source string.
+ * @str: Destination string.
+ */
+void _strcpy(char *s, char *str)
+{
+    int i;
+
+    if (!s || !str)
+        return;
+
+    for (i = 0; s[i] != 0; i++)
+    {
+        str[i] = s[i];
+    }
+    str[i] = '\0';
+}
+
+/**
+ * new_dog - Creates a new dog structure.
+ * @name: Pointer to the dog's name.
+ * @age: The dog's age.
+ * @owner: Pointer to the dog's owner's name.
+ *
+ * Return: Pointer to the newly created dog structure,
+ * or NULL if memory allocation fails.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
     char *sptr1, *sptr2;
-
     dog_t *ptr = (dog_t *) malloc(sizeof(dog_t));
+
     if (!ptr)
-        return NULL;
+        return (NULL);
 
     ptr->age = age;
+
     if (name)
     {
         sptr1 = (char *) malloc(sizeof(char) * (counrstr(name) + 1));
@@ -22,14 +68,13 @@ dog_t *new_dog(char *name, float age, char *owner)
         else
         {
             free(ptr);
-            return NULL;
+            return (NULL);
         }
-        
     }
-    
+
     if (owner)
     {
-        sptr2 = (char *) malloc(sizeof(char) * counrstr(owner) + 1);
+        sptr2 = (char *) malloc(sizeof(char) * (counrstr(owner) + 1));
         if (sptr2)
         {
             _strcpy(owner, sptr2);
@@ -39,36 +84,11 @@ dog_t *new_dog(char *name, float age, char *owner)
         {
             free(ptr);
             if (name)
-    {
-            free(sptr1);
-    }
-            return NULL;
+            {
+                free(sptr1);
+            }
+            return (NULL);
         }
     }
-    return ptr;
-}
-
-int counrstr(char *str)
-{
-    int i;
-    if (!str)
-    {
-        return 0;
-    }
-    
-    for (i = 0; str[i] != 0; i++)
-        continue;
-    return i;
-}
-
-void _strcpy(char *s, char *str)
-{
-    int i;
-    if (!s || !str)
-        return;
-    for (i = 0; s[i] != 0; i++)
-        {
-            str[i] = s[i];
-        }
-        str[i] = '\0';
+    return (ptr);
 }
