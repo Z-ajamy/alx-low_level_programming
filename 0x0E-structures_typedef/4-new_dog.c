61b6c599020df04fb7b6ef574cd29efa74bc720f
Z-ajamy <abdorahman0283@gmail.com>
2025-03-16 17:53:44 +0200

    My name is Ghost, I am 4.75, and my owner is Jon Snow

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,15 +8,15 @@
  */
 int counrstr(char *str)
 {
-    int i;
+	int i;
 
-    if (!str)
-        return (0);
+	if (!str)
+		return (0);
 
-    for (i = 0; str[i] != 0; i++)
-        continue;
+	for (i = 0; str[i] != 0; i++)
+		continue;
 
-    return (i);
+	return (i);
 }
 
 /**
@@ -26,16 +26,16 @@ int counrstr(char *str)
  */
 void _strcpy(char *s, char *str)
 {
-    int i;
+	int i;
 
-    if (!s || !str)
-        return;
+	if (!s || !str)
+		return;
 
-    for (i = 0; s[i] != 0; i++)
-    {
-        str[i] = s[i];
-    }
-    str[i] = '\0';
+	for (i = 0; s[i] != 0; i++)
+	{
+		str[i] = s[i];
+	}
+	str[i] = '\0';
 }
 
 /**
@@ -49,46 +49,46 @@ void _strcpy(char *s, char *str)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    char *sptr1, *sptr2;
-    dog_t *ptr = (dog_t *) malloc(sizeof(dog_t));
+	char *sptr1, *sptr2;
+	dog_t *ptr = (dog_t *) malloc(sizeof(dog_t));
 
-    if (!ptr)
-        return (NULL);
+	if (!ptr)
+		return (NULL);
 
-    ptr->age = age;
+	ptr->age = age;
 
-    if (name)
-    {
-        sptr1 = (char *) malloc(sizeof(char) * (counrstr(name) + 1));
-        if (sptr1)
-        {
-            _strcpy(name, sptr1);
-            ptr->name = sptr1;
-        }
-        else
-        {
-            free(ptr);
-            return (NULL);
-        }
-    }
+	if (name)
+	{
+		sptr1 = (char *) malloc(sizeof(char) * (counrstr(name) + 1));
+		if (sptr1)
+		{
+			_strcpy(name, sptr1);
+			ptr->name = sptr1;
+		}
+		else
+		{
+			free(ptr);
+			return (NULL);
+		}
+	}
 
-    if (owner)
-    {
-        sptr2 = (char *) malloc(sizeof(char) * (counrstr(owner) + 1));
-        if (sptr2)
-        {
-            _strcpy(owner, sptr2);
-            ptr->owner = sptr2;
-        }
-        else
-        {
-            free(ptr);
-            if (name)
-            {
-                free(sptr1);
-            }
-            return (NULL);
-        }
-    }
-    return (ptr);
+	if (owner)
+	{
+		sptr2 = (char *) malloc(sizeof(char) * (counrstr(owner) + 1));
+		if (sptr2)
+		{
+			_strcpy(owner, sptr2);
+			ptr->owner = sptr2;
+		}
+		else
+		{
+			free(ptr);
+			if (name)
+			{
+				free(sptr1);
+			}
+			return (NULL);
+		}
+	}
+	return (ptr);
 }
