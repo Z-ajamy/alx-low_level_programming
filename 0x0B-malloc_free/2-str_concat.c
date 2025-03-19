62e7656aab1f05bf7e7e2d3b9af5e7386988a7aa
Z-ajamy <abdorahman0283@gmail.com>
2025-03-19 19:41:57 +0200

    0

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,8 +5,8 @@ char *str_concat(char *s1, char *s2)
 {
     int size1, size2, i, j;
     char *ptr;
-    if (!s1 || !s2)
-        return NULL;
+    
+    
     size1 = _strlen(s1);
     size2 = _strlen(s2);
     ptr = (char *)malloc((size1 + size2 + 1) * sizeof(char));
@@ -14,8 +14,9 @@ char *str_concat(char *s1, char *s2)
         return NULL;
     for (i = 0; i < size1; i++)
         ptr[i] = s1[i];
-    for (j = 0; j <= size2; j++, i++)
+    for (j = 0; j < size2; j++, i++)
         ptr[i] = s2[j];
+    ptr[i] = '\0';
     return ptr;
     
     
@@ -25,6 +26,12 @@ char *str_concat(char *s1, char *s2)
 int _strlen(char *str)
 {
     int i = 0;
+
+    if (!str)
+    {
+        return i;
+    }
+    
 
     while (str[i])
         i++;
