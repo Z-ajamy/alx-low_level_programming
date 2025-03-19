47198097a6fe1fa657305b3bac6dfeef221acb31
Z-ajamy <abdorahman0283@gmail.com>
2025-03-19 20:13:06 +0200

    Allocates a 2D array of integers initialized to 0.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,19 +1,31 @@
 #include "main.h"
 
+/**
+ * alloc_grid - Allocates a 2D array of integers initialized to 0.
+ * @width: Number of columns.
+ * @height: Number of rows.
+ *
+ * Return: Pointer to the allocated grid, or NULL on failure.
+ */
 int **alloc_grid(int width, int height)
 {
     int **ptr;
     int i, j;
-    if (width <= 0 || width <= 0)
+
+    /* Validate input dimensions */
+    if (width <= 0 || height <= 0)
         return NULL;
 
-    ptr = (int **) malloc(sizeof(int *) * height);
+    /* Allocate memory for row pointers */
+    ptr = malloc(sizeof(int *) * height);
     if (!ptr)
         return NULL;
-    
+
+    /* Allocate memory for each row */
     for (i = 0; i < height; i++)
     {
-        ptr[i] = (int *) malloc(sizeof(int) * width);
+        ptr[i] = malloc(sizeof(int) * width);
+
         if (!ptr[i])
         {
             for (j = i - 1; j >= 0; j--)
@@ -23,20 +35,16 @@ int **alloc_grid(int width, int height)
             free(ptr);
             return NULL;
         }
-        
     }
 
+    /* Initialize all elements to 0 */
     for (i = 0; i < height; i++)
     {
         for (j = 0; j < width; j++)
         {
             ptr[i][j] = 0;
         }
-        
     }
+
     return ptr;
-    
-    
-    
-    
 }
