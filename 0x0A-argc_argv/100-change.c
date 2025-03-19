cc68fa2dfad91076684bc63c72470639d827fddb
Z-ajamy <abdorahman0283@gmail.com>
2025-03-19 02:58:13 +0200

    0

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,77 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-int is_num(char *str);
-int is_pos(char *str);
+#include "main.h"
 
-
+/**
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
-    unsigned int a, i = 0;
-    if (argc != 2 || !is_num(argv[1]))
-    {
-        printf("Error\n");
-        exit(1);
-    }
-    if (!is_pos(argv[1]))
-    {
-        printf("0\n");
-        exit(1);
-    }
-    a = atoi(argv[1]);
-    if (a >= 25)
-    {
-        i += a/25;
-        a %= 25;           
-    }
-     if (a >= 10)
-    {
-        i += a/10;
-        a %= 10;
-    }
-     if (a >= 5)
-    {
-        i += a/5;
-        a %= 5;
-    }
-    if (a >= 2)
-    {
-        i += a/2;
-        a %= 2;
-    }
-    if (a >= 1)
-    {
-        i += a/1;
-        a %= 1;
-    }
-    
-        
-    printf("%d\n", i);
-    return 0;
-    
-    
-    
-}
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-int is_num(char *str)
-{
-    int i;
+	num = atoi(argv[1]);
+	result = 0;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] < '0' || str[i] > '9')
-        {
-            return (0);
-        }
-    }
-    return (1);
-}
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-int is_pos(char *str)
-{
-    if (str[1] == '-')
-    {
-        return (0);
-    }
-    return (1);
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+
+	printf("%d\n", result);
+	return (0);
 }
