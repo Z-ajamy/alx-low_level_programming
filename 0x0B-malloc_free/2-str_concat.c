c17c4c155dfbd9b1e97762ce786fc36b2cbcff37
Z-ajamy <abdorahman0283@gmail.com>
2025-03-19 19:44:00 +0200

    Concatenates two strings into newly allocated memory.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,40 +1,52 @@
-#include"main.h"
-int _strlen(char *str);
+#include "main.h"
 
-char *str_concat(char *s1, char *s2)
-{
-    int size1, size2, i, j;
-    char *ptr;
-    
-    
-    size1 = _strlen(s1);
-    size2 = _strlen(s2);
-    ptr = (char *)malloc((size1 + size2 + 1) * sizeof(char));
-    if (!ptr)
-        return NULL;
-    for (i = 0; i < size1; i++)
-        ptr[i] = s1[i];
-    for (j = 0; j < size2; j++, i++)
-        ptr[i] = s2[j];
-    ptr[i] = '\0';
-    return ptr;
-    
-    
-    
-}
-
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The input string.
+ *
+ * Return: The length of the string.
+ */
 int _strlen(char *str)
 {
     int i = 0;
 
     if (!str)
-    {
-        return i;
-    }
-    
+        return 0;
 
     while (str[i])
         i++;
 
     return i;
+}
+
+/**
+ * str_concat - Concatenates two strings into newly allocated memory.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: Pointer to the newly allocated concatenated string, or NULL on failure.
+ */
+char *str_concat(char *s1, char *s2)
+{
+    int size1, size2, i, j;
+    char *ptr;
+
+    size1 = _strlen(s1);
+    size2 = _strlen(s2);
+
+    ptr = malloc((size1 + size2 + 1) * sizeof(char));
+    if (!ptr)
+        return NULL;
+
+    /* Copy first string */
+    for (i = 0; i < size1; i++)
+        ptr[i] = s1[i];
+
+    /* Copy second string */
+    for (j = 0; j < size2; j++, i++)
+        ptr[i] = s2[j];
+
+    ptr[i] = '\0'; /* Null-terminate the new string */
+
+    return ptr;
 }
