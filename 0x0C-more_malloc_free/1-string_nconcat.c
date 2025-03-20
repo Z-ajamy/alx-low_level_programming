8b88c05ee55da9802b61e18384ee91d254b46d55
Z-ajamy <abdorahman0283@gmail.com>
2025-03-20 17:56:51 +0200

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+int _strlen(char *s);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    int size1, size2;
+    char *ptr;
+    int i, j;
+    if (!s1)
+        s1 = "";
+    if (!s2)
+    {
+        s2 = "";
+    }
+    if (n == 0)
+        return s1;
+    size1 = _strlen(s1);
+    size2 = _strlen(s2);
+    if ((unsigned int)size2 < n)
+        n = size2;
+    ptr = malloc(sizeof(*ptr) * (size1 + n + 1));
+    if (!ptr)
+        return NULL;
+    for (i = 0; i < size1; i++)
+    {
+        ptr[i] = s1[i];
+    }
+    for (j = 0; (unsigned int)j < n; i++, j++)
+    {
+        ptr[i] = s2[j];
+    }
+    ptr[i] = '\0';
+    return ptr;
+    
+
+}
+
+int _strlen(char *s)
+{
+    int i = 0;
+    if (s == NULL)
+        return 0;
+    while (s[i])
+    {
+        i++;
+    }
+    return i;
+}
