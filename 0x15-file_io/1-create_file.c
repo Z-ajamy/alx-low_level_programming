2d06c0a2fe80d37e80f780bd7ab417f0ce7850b7
Z-ajamy <abdorahman0283@gmail.com>
2025-03-28 17:19:31 +0200

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,5 +2,30 @@
 
 int create_file(const char *filename, char *text_content)
 {
+    int fd, i = 0, n;
+
+    if (!filename)
+        return -1;
+    fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+    if (fd == -1)
+        return -1;
+    if(text_content == NULL)
+    {
+        close(fd);
+        return -1;
+    }
+    while (text_content[i])
+    {
+        i++;
+    }
+    
+    n = write(fd, text_content, i);
+    if (i != n)
+    {
+        close(fd);
+        return -1;
+    }
+    return 1;
+    
     
 }
