0a3528d2c85c879d20c13392a983a8aed18e5195
Z-ajamy <abdorahman0283@gmail.com>
2025-03-28 17:32:08 +0200

    Creates a file and writes text content to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,19 +1,29 @@
-#include"main.h"
+#include "main.h"
 
+/**
+ * create_file - Creates a file and writes text content to it
+ * @filename: The name of the file to create
+ * @text_content: The content to write into the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
     int fd, i = 0, n;
 
     if (!filename)
-        return -1;
+        return (-1);
+    
     fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
     if (fd == -1)
-        return -1;
-    if(text_content == NULL)
+        return (-1);
+    
+    if (text_content == NULL)
     {
         close(fd);
-        return 1;
+        return (1);
     }
+    
     while (text_content[i])
     {
         i++;
@@ -23,9 +33,8 @@ int create_file(const char *filename, char *text_content)
     if (i != n)
     {
         close(fd);
-        return -1;
+        return (-1);
     }
-    return 1;
     
-    
+    return (1);
 }
