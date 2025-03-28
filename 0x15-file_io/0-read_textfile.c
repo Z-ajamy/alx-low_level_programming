09694f448e1c9653a4a68d17688d08923e8b1e00
Z-ajamy <abdorahman0283@gmail.com>
2025-03-28 16:37:44 +0200

    Reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,42 @@
-#include"main.h"
+#include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: The name of the file to read
+ * @letters: The number of letters to read and print
+ *
+ * Return: The actual number of letters it could read and print, or 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     int i, fd, n;
     char *ptr;
 
     if (!filename)
-        return 0;
-    fd = open(filename, O_RDONLY);
-
-    if (fd == -1)
-        return 0;
+        return (0);
     
-    ptr = (char*)malloc(sizeof(char) * letters);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+    
+    ptr = (char *)malloc(sizeof(char) * letters);
     if (!ptr)
     {
         close(fd);
-        return 0;
+        return (0);
     }
+    
     i = read(fd, ptr, letters);
     if (i == -1)
     {
         close(fd);
         free(ptr);
-        return 0;
+        return (0);
     }
+    
     n = write(STDOUT_FILENO, ptr, i);
     
     close(fd);
     free(ptr);
-    return n;    
+    return (n);
 }
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,6 @@
+#include"main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+    
+}
