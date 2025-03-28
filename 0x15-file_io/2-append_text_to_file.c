6af5239d1c5f27187f2e1788e1210277e0bc7d88
Z-ajamy <abdorahman0283@gmail.com>
2025-03-28 17:47:58 +0200

    ?

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,6 +8,13 @@ int append_text_to_file(const char *filename, char *text_content)
     fd = open(filename, O_WRONLY | O_APPEND);
     if (fd == -1)
         return -1;
+
+    if (!text_content)
+    {
+        close(fd);
+        return 1;
+    }
+    
     while (text_content[i])
         i++;
 
