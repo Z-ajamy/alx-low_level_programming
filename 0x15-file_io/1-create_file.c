6b6a5f14a1c99b44a8ebba3921131667f5ab6f99
Z-ajamy <abdorahman0283@gmail.com>
2025-03-28 17:24:49 +0200

    ؟

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
     if(text_content == NULL)
     {
         close(fd);
-        return -1;
+        return 1;
     }
     while (text_content[i])
     {
--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -3,20 +3,22 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code .
  *
+ * @ac: Argumetns counter
+ * @av: Arguments array
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-    int res;
+	int res;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	if (ac < 2)
+	{
+		printf("Usage: %s filename\n", av[0]);
+		return (1);
+	}
+	res = create_file(av[1], NULL);
+	printf("%i\n", res);
+	return (0);
 }
