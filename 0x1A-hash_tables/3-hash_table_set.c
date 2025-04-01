3a1d1432c1b1d3a00d525c6888ebad6f90164727
Z-ajamy <abdorahman0283@gmail.com>
2025-04-02 00:56:51 +0200

    0

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -39,27 +39,36 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 hash_node_t *creat_item(const char *key, const char *val)
 {
+    long unsigned int i;
     hash_node_t *item = NULL;
 
-    item = (hash_node_t *)calloc(1, sizeof(hash_node_t));
+    item = (hash_node_t *)malloc(sizeof(hash_node_t));
     if (!item)
         return NULL;
     
     item->next = NULL;
-    item ->key = (char *)calloc(1, (sizeof(char) * strlen(key)) + 1);
+    item ->key = (char *)malloc((sizeof(char) * strlen(key)) + 1);
     if (!(item->key))
     {
         free(item);
         return NULL;
     }
-    item->value = (char *)calloc(1, sizeof(char) * strlen(val) + 1);
+    for (i = 0; i < ((sizeof(char) * strlen(key)) + 1); i++)
+    {
+        item->key[i] = 0;
+    }
+    
+    item->value = (char *)malloc(sizeof(char) * strlen(val) + 1);
     if (!(item->value))
     {
         free(item->key);
         free(item);
         return NULL;
     }
-    
+    for (i = 0; i < (sizeof(char) * strlen(val) + 1); i++)
+    {
+        item->value[i] = 0;
+    }
     strcpy(item->key, key);
     strcpy(item->value, val);
 
