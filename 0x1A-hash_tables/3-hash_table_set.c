776c70795a833810e4144b05312f7bc3b917ad04
Z-ajamy <abdorahman0283@gmail.com>
2025-04-02 00:51:56 +0200

    ?

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     {
         if (strcmp(curr->key, item->key) == 0)
         {
-            strcpy (curr->value, key);
+            strcpy (curr->value, value);
             free_item(item);
             return 1;
         }
