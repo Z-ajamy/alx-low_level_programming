7135f0e444f448b87cd4f1e43fe013dde915dfed
Z-ajamy <abdorahman0283@gmail.com>
2025-04-01 21:19:42 +0200

    implementation of the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,7 +8,7 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash = 5381;
-    char c;
+    int c;
     while ((c = *(str++)))
     {
         hash = ((hash << 5) + hash) + c;
--- 0x1A-hash_tables/1-main.c
+++ 0x1A-hash_tables/1-main.c
@@ -1,22 +1,17 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    char *s;
+	char *s;
 
-    s = "cisfun";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    s = "Don't forget to tweet today";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    s = "98";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    return (EXIT_SUCCESS);
+	s = "\x07\x08\x09\x0A\x0BCisfun\x0C";
+	printf("%lu\n", hash_djb2((unsigned char *)s));
+	return (EXIT_SUCCESS);
 }
