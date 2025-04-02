f9adfa8d465214b7b2be2a3b5c2a849095d77925
Z-ajamy <abdorahman0283@gmail.com>
2025-04-02 16:55:22 +0200

    Prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,27 +1,46 @@
-#include"hash_tables.h"
+#include "hash_tables.h"
 
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: Pointer to the hash table
+ *
+ * Description: This function prints the key-value pairs stored in
+ * the hash table in a dictionary-like format.
+ */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *ptr;
-    unsigned long int i; 
-    char flag = 1;
+    hash_node_t *ptr;      /* Pointer to traverse the linked list */
+    unsigned long int i;   /* Loop variable to iterate through the array */
+    char flag = 1;         /* Flag to control comma placement */
 
+    /* Check if the hash table is NULL */
     if (!ht)
         return;
+
+    /* Print the opening brace for the dictionary format */
     printf("{");
+
+    /* Iterate through the hash table array */
     for (i = 0; i < ht->size; i++)
     {
         ptr = ht->array[i];
+
+        /* Traverse the linked list at the current index */
         while (ptr)
         {
+            /* Print a comma before printing a new key-value pair */
+            if (!flag)
+                printf(", ");
+            flag = 0;
             
-                if (!flag)
-                    printf(", ");
-                flag = 0;
-                printf("\'%s\': \'%s\'", ptr->key, ptr->value);
+            /* Print the key-value pair in dictionary format */
+            printf("'%s': '%s'", ptr->key, ptr->value);
             
+            /* Move to the next node in the linked list */
             ptr = ptr->next;
         }
     }
+
+    /* Print the closing brace for the dictionary format */
     printf("}\n");
 }
