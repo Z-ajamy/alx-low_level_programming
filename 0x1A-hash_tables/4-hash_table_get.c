85b9a5874dbfb8f6c64c37e7d422f59c3a840644
Z-ajamy <abdorahman0283@gmail.com>
2025-04-02 16:19:34 +0200

    Retrieves a value associated with a key in a hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,26 +1,44 @@
-#include"hash_tables.h"
+#include "hash_tables.h"
 
+/**
+ * hash_table_get - Retrieves a value associated with a key in a hash table
+ * @ht: Pointer to the hash table
+ * @key: The key to search for
+ *
+ * Return: The value associated with the key, or NULL if the key is not found
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    int index;
-    hash_node_t *ptr, *p;
+    int index;              /* Variable to store the computed index */
+    hash_node_t *ptr, *p;   /* Pointers to traverse the linked list */
 
-
+    /* Validate input parameters */
     if (!ht || !key || key[0] == '\0')
-        return NULL;
+        return (NULL);
+
+    /* Compute the index of the key in the hash table */
     index = key_index((const unsigned char *)key, ht->size);
     ptr = ht->array[index];
+
+    /* If no element exists at the computed index, return NULL */
     if (!ptr)
-        return NULL;
+        return (NULL);
+
+    /* Traverse the linked list to search for the key */
     while (ptr)
     {
         p = ptr;
+
+        /* If the key matches, return its corresponding value */
         if (strcmp(ptr->key, key) == 0)
         {
-            return ptr->value;
+            return (ptr->value);
         }
+
+        /* Move to the next node in the linked list */
         ptr = p->next;
     }
-    return NULL;
-    
+
+    /* If key is not found, return NULL */
+    return (NULL);
 }
