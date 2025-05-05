4500a52d1256903866836838ff32cfaeca24fa8b
Z-ajamy <abdorahman0283@gmail.com>
2025-05-06 02:38:45 +0300

    betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,14 +1,23 @@
-#include"lists.h"
+#include "lists.h"
+#include <stdlib.h> /* For free */
 
+/**
+ * free_list - Frees a list_t list
+ *
+ * @head: Pointer to the head of the list
+ *
+ * Description: This function frees each node of the list and the memory
+ *              allocated for the string in each node.
+ */
 void free_list(list_t *head)
 {
-    list_t *ptr = NULL;
-    while (head)
-    {
-        free(head->str);
-        ptr = head->next;
-        free(head);
-        head = ptr;
-    }
-    
+	list_t *ptr = NULL;
+
+	while (head)
+	{
+		free(head->str);
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
 }
