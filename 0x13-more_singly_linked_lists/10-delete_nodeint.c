bbdc1937699edb93163c7120401133326e4d9479
Z-ajamy <abdorahman0283@gmail.com>
2025-06-19 03:52:55 +0300

    Deletes the node at index of a listint_t list !

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,8 +20,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
     if (index == 0)
     {
+        *head = p->next;
         free(ptr);
-        *head = ptr->next;
         return (1);
     }
 
@@ -36,7 +36,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
         }
     }
 
-    if (i < index)
+    if (i < index || !ptr)
         return (-1);
 
     p->next = ptr->next;
